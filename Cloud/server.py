6c1f359952c2412a677624ce7233dcf2962c5ab4
Hung Vu <hung.h.vu@aalto.fi>
2023-03-06 18:10:31 +0200

    Added PATCH request

--- Cloud/server.py
+++ Cloud/server.py
@@ -24,11 +24,16 @@ sensor_put_args = reqparse.RequestParser()
 sensor_put_args.add_argument('temp', type=str, help='Temperature not specified', required=True)
 sensor_put_args.add_argument('humid', type=str, help='Humidity not specified', required=True)
 
+# argument parser for update
+sensor_update_args = reqparse.RequestParser()
+sensor_update_args.add_argument('temp', type=str, help='Temperature not specified', required=False)
+sensor_update_args.add_argument('humid', type=str, help='Humidity not specified', required=False)
+
 
 # fields for serialization
 resource_fields = {
     'timestamp': fields.String,
-    'temperature': fields.String,
+    'temperature': fields.Integer,
     'humidity':fields.Integer
 }
 
@@ -39,6 +44,7 @@ class SensorData(Resource):
     def get(self, time):
         # query and return the data
         result = SensorDataModel.query.filter_by(timestamp=time).first()
+        # print("Received GET request for timestamp " + time)
         if not result:
             abort(404, message="Timestamp not found")
         return result
@@ -47,35 +53,36 @@ class SensorData(Resource):
     def put(self, time):
         args = sensor_put_args.parse_args()
         
-        # check whether the student ID is taken or not
+        # check whether the timestamp is taken or not
         result = SensorDataModel.query.filter_by(timestamp=time).first()
+        # print("Received PUT request for timestamp " + time + " with arguments: " + "{temp:" + args['temp'] + ",humid:" \
+        #       + args['humid'] + '}')
         if result:
-            abort(409, message='Timestamp already exist.')
+            abort(409, message='Timestamp already exist')
 
         # create a student object and add it to the session
         data = SensorDataModel(timestamp=time, temperature=args['temp'], humidity=args['humid'])
         db.session.add(data)
         db.session.commit()
-        print('received request')
         return data, 201
     
-    # @marshal_with(resource_fields)
-    # def patch(self, studentID):
-    #     args = sensor_put_args.parse_args()
+    @marshal_with(resource_fields)
+    def patch(self, time):
+        args = sensor_update_args.parse_args()
 
-    #     # check whether the student ID exist or not
-    #     result = SensorDataModel.query.filter_by(id=studentID).first()
-    #     if not result:
-    #         abort(404, message='Student ID not found')
+        # check whether the timestamp exist or not
+        result = SensorDataModel.query.filter_by(timestamp=time).first()
+        if not result:
+            abort(404, message='Timestamp not found')
         
-    #     if args['name']:
-    #         result.name = args['name']
-    #     if args['age']:
-    #         result.age = args['age']
+        if args['temp']:
+            result.temperature = args['temp']
+        if args['humid']:
+            result.humidity = args['humid']
 
-    #     db.session.commit()
+        db.session.commit()
 
-    #     return result, 201
+        return result, 201
 
     # @marshal_with(resource_fields)    
     # def delete(self, studentID):
@@ -99,4 +106,4 @@ api.add_resource(SensorData, "/sensordata/<string:time>")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run()
