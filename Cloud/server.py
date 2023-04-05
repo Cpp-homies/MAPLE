d843d72dabe5184c74837d5fb7cc71e338aaf4b6
Hung Vu <hung.h.vu@aalto.fi>
2023-04-05 13:09:46 +0300

    Added user ID support for GET, PUT, and get_datatable methods

--- Cloud/server.py
+++ Cloud/server.py
@@ -24,12 +24,13 @@ db = SQLAlchemy(app)
 # create the schema for the student
 class SensorDataModel(db.Model):
     timestamp = db.Column(db.String(20), primary_key=True)
+    user_id = db.Column(db.String, primary_key=True)
     temperature = db.Column(db.Float, nullable=False)
     air_humidity = db.Column(db.Float, nullable=False)
     soil_humidity = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
-        return f"Data(time={self.timestamp}, temp={self.temperature}, air_humid={self.air_humidity}, soil_humid={self.soil_humidity})"
+        return f"Data(time={self.timestamp}, user={self.user_id}, temp={self.temperature}, air_humid={self.air_humidity}, soil_humid={self.soil_humidity})"
     
 # db.create_all()
 
@@ -42,6 +43,7 @@ sensor_put_args.add_argument('soil_humid', type=str, help='Soil humidity not spe
 # argument parser for "live data" put
 sensor_live_put_args = reqparse.RequestParser()
 sensor_live_put_args.add_argument('time', type=str, help='Timestamp not specified', required=True)
+# sensor_live_put_args.add_argument('user_id', type=str, help='User ID not specified', required=True)
 sensor_live_put_args.add_argument('temp', type=str, help='Temperature not specified', required=True)
 sensor_live_put_args.add_argument('air_humid', type=str, help='Air humidity not specified', required=True)
 sensor_live_put_args.add_argument('soil_humid', type=str, help='Soil humidity not specified', required=True)
@@ -56,6 +58,7 @@ sensor_update_args.add_argument('soil_humid', type=str, help='Humidity not speci
 # fields for serialization
 resource_fields = {
     'timestamp': fields.String,
+    'user_id': fields.String,
     'temperature': fields.Float,
     'air_humidity':fields.Float,
     'soil_humidity':fields.Float
@@ -79,6 +82,12 @@ def wait_until(cond, timeout):
         return False
     return True
 
+# class Clients():
+#     def __init__(self, user_id):
+#         self.user_id = user_id
+#         self.live_data
+        
+
 # test resource
 class SensorData(Resource):
     live_data = {} # A buffer to hold live data from the device
@@ -86,11 +95,11 @@ class SensorData(Resource):
 
     # test get function that return a dictionary with "timestamp", "temperature", "air_humidity", and "soil_humidity"
     @marshal_with(resource_fields)
-    def get(self, timestamp):
+    def get(self, timestamp, user_id):
         # Check if this is a request for live data or not
         if timestamp == "live":
             # Ask the ESP to update the live data (function executed aynchronously in the background)
-            update_status = asyncio.run(self.update_live_data())
+            update_status = asyncio.run(self.update_live_data(user_id))
             # print(update_status)
             if not update_status:
                 # Inform the sender that the request has timed out
@@ -102,17 +111,24 @@ class SensorData(Resource):
                 return self.live_data
 
         else:
-            # query and return the data
-            result = SensorDataModel.query.filter_by(timestamp=timestamp).first()
-            # print("Received GET request for timestamp " + time)
+             # Query data by user ID
+            results = SensorDataModel.query.filter_by(user_id=user_id).all()
+
+            # Check if user ID exists
+            if not results:
+                abort(404, message="User ID not found")
+
+            # Filter data by timestamp
+            result = next((item for item in results if item.timestamp == timestamp), None)
+
+            # Check if timestamp exists
             if not result:
                 abort(404, message="Timestamp not found")
-            return result
     
     # update the live data from the ESP
-    async def update_live_data(self):
+    async def update_live_data(self, user_id):
          # Add the request to the pending request list for the ESP
-        esp_reqs.append({"type":ESP_Req_Code.LIVE_DATA.value})
+        esp_reqs.append({"user_id": user_id, "type":ESP_Req_Code.LIVE_DATA.value})
 
         # Wait until the ESP respond or timeout has been reached
         req_succeed = wait_until(self.esp_req_served, esp_req_timeout)
@@ -123,13 +139,14 @@ class SensorData(Resource):
             return True
         
     @marshal_with(resource_fields)
-    def put(self, timestamp):
+    def put(self, timestamp, user_id):
         try:
             if timestamp == "live":
                 args = sensor_live_put_args.parse_args()
 
-                # Add the data to the dictionary reference
+                # Add the data to the live_data dictionary
                 self.live_data["timestamp"] = args['time']
+                self.live_data["user_id"] = user_id
                 self.live_data["temperature"] = float(args["temp"])
                 self.live_data["air_humidity"] = float(args["air_humid"])
                 self.live_data["soil_humidity"] = float(args["soil_humid"])
@@ -140,14 +157,14 @@ class SensorData(Resource):
             
             args = sensor_put_args.parse_args()
             # check whether the timestamp is taken or not
-            result = SensorDataModel.query.filter_by(timestamp=timestamp).first()
+            result = SensorDataModel.query.filter_by(timestamp=timestamp, user_id=user_id).first()
             # print("Received PUT request for timestamp " + time + " with arguments: " + "{temp:" + args['temp'] + ",humid:" \
             #       + args['humid'] + '}')
             if result:
                 abort(409, message='Timestamp already exist')
 
             # create a SensorDataModel object and add it to the session
-            data = SensorDataModel(timestamp=timestamp, temperature=float(args['temp']), air_humidity=float(args['air_humid']), soil_humidity=float(args['soil_humid']))
+            data = SensorDataModel(timestamp=timestamp, user_id=user_id, temperature=float(args['temp']), air_humidity=float(args['air_humid']), soil_humidity=float(args['soil_humid']))
             db.session.add(data)
             db.session.commit()
             return data, 201
@@ -156,12 +173,12 @@ class SensorData(Resource):
             abort(400, message="Invalid arguments")
     
     @marshal_with(resource_fields)
-    def patch(self, timestamp):
+    def patch(self, timestamp, user_id):
         try:
             args = sensor_update_args.parse_args()
 
             # check whether the timestamp exist or not
-            result = SensorDataModel.query.filter_by(timestamp=timestamp).first()
+            result = SensorDataModel.query.filter_by(timestamp=timestamp, user_id=user_id).first()
             if not result:
                 abort(404, message='Timestamp not found')
             
@@ -179,10 +196,10 @@ class SensorData(Resource):
             # Catch and handle ValueError exceptions
             abort(400, message="Invalid arguments")
     
-    @app.route('/datatable', methods=['GET'])
-    def get_data_table():
+    @app.route('/<string:user_id>/datatable', methods=['GET'])
+    def get_data_table(user_id):
         # Query all data rows from the database
-        rows = SensorDataModel.query.all()
+        rows = SensorDataModel.query.filter_by(user_id=user_id).all()
 
         # Convert the data rows into a list of dictionaries
         data_list = []
@@ -199,8 +216,8 @@ class SensorData(Resource):
         return jsonify(data_list)
     
     # GET method to get the next request for the ESP to execute
-    @app.route('/request-to-esp', methods=['GET'])
-    def get_esp_request():
+    @app.route('/<string:user_id>/request-to-esp', methods=['GET'])
+    def get_esp_request(user_id):
         if esp_reqs:
             return esp_reqs.pop()
         return {"type": ESP_Req_Code.NO_REQ.value}
@@ -224,7 +241,7 @@ class SensorData(Resource):
     # def post(self):
     #     return {'data':'Posted'}
  
-api.add_resource(SensorData, "/sensordata/<string:timestamp>")
+api.add_resource(SensorData, "/sensordata/<string:user_id>/<string:timestamp>")
 
 
 if __name__ == "__main__":
